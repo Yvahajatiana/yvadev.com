9fbcb29e8369dc7bf9945ec59b1d75feed86d164
YvaDev <hajatianay@proximity.bbdo.fr>
2018-03-27 22:53:27 +0400

    Feature: Implement Login and Registration

---
+++ Web/Controllers/AccountController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Yvadev.Infrastructure.EF.Models;
+using Yvadev.Web.ViewModels;
+
+namespace Web.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+        private readonly IMapper mapper;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IMapper mapper)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(string returnUrl = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        {
+            ViewData["returnUrl"] = returnUrl;
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            signInManager.PasswordSignInAsync(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register(string returnUrl = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([FromBody]RegisterViewModel model, string returnUrl = null)
+        {
+            ViewData["returnUrl"] = returnUrl;
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var newUser = mapper.Map<AppUser>(model);
+
+            var result = userManager.CreateAsync(newUser, model.Password);
+            if (!result.Result.Succeeded) return new BadRequestObjectResult(ModelState);
+
+            return new OkObjectResult("Account Created");
+        }
+    }
+}
--- Web/Startup.cs
+++ Web/Startup.cs
@@ -12,6 +12,10 @@ using Yvadev.Domain.Contracts;
 using Yvadev.Infrastructure.EF.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Yvadev.Infrastructure.EF.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using AutoMapper;
 
 namespace Yvadev.Web
 {
@@ -27,7 +31,6 @@ namespace Yvadev.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             services.AddDbContext<ApplicationContext>(options
                 => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
                 b => b.MigrationsAssembly("Yvadev.Infrastructure.EF")));
@@ -37,6 +40,24 @@ namespace Yvadev.Web
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             services.AddIdentity<AppUser, IdentityRole>()
                 .AddEntityFrameworkStores<IdentityContext>().AddDefaultTokenProviders();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddCookie()
+                .AddJwtBearer(JwtBearerOptions =>
+                {
+                    JwtBearerOptions.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateActor = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Token:Issuer"],
+                        ValidAudience = Configuration["Token:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Token:Key"]))
+                    };
+                });
+            services.AddAutoMapper();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +72,8 @@ namespace Yvadev.Web
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            app.UseAuthentication();
 
             app.UseStaticFiles();
         }
