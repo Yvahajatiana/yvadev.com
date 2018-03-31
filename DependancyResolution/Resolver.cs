cc6a361d59527125ac7df3fa03a9c8b98940cdab
YvaDev <hajatianay@proximity.bbdo.fr>
2018-03-31 23:06:24 +0400

    Feat: add dependency resolution project

---
+++ DependancyResolution/Resolver.cs
@@ -0,0 +1,20 @@
+﻿namespace Yvadev.DependencyResolution
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Yvadev.Domain.Contracts;
+    using Yvadev.Domain.Services;
+    using Yvadev.Infrastructure.EF.Repositories;
+
+    public static class Resolver
+    {
+        public static void Register(IServiceCollection services)
+        {
+            /*
+             * Dependancy Injection
+             */
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped<IPostService, PostService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+        }
+    }
+}
